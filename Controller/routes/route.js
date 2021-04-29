f61b899604c62a05997e6922ac8f682e4c92e669
Manisha3112 <greenyforever31@gmail.com>
2021-04-29 17:29:04 +0530

    changes made on index

--- Controller/routes/route.js
+++ Controller/routes/route.js
@@ -2,8 +2,6 @@ const { decodeBase64 } = require('bcryptjs');
 var express= require('express');
 var connection=require('../../model/database.js');
 const router=express.Router()
-
-
 router.get('/login',function(req,res){
     console.log("WELCOME!!");
     res.render("vlogin");
@@ -18,7 +16,6 @@ router.post('/validate',function(req,res){
              if (err)
                  throw err;
              if (results) {
-                 
                  connection.query('select password from voter_login where email like ? and password like ?)', [email, pwd], (_err, data) => {
                     function resolveAfter2Seconds() {
                         return new Promise(resolve => {
@@ -27,7 +24,6 @@ router.post('/validate',function(req,res){
                           }, 2000);
                         });
                       }
-                      
                       async function asyncCall() {
                         console.log('calling');
                         const result = await resolveAfter2Seconds();
@@ -48,34 +44,6 @@ catch(err){
     connection.rollback();
 }
 })
-// router.get('/login',function(req,res){
-//     console.log("WELCOME!!");
-//     res.render("vlogin");
-// })
-// router.post('/validate',function(req,res){
-//     var email=req.body.Email; 
-//     var pwd=req.body.password;
-//     connection.query('select email from voter_login where email like ?',[email],(err,results)=>{
-//        if (err) throw err;
-       
-//           if(results[0].email==email){
-//             connection.query('select password from voter_login where email like ?',[email],(_err,result)=>{
-//                 if(result[0].password==pwd)
-//                 {
-//                     console.log(result[0].password);
-                    
-//                         res.render("vote-reg", {userData: result });
-                
-//              }
-            
-//         })
-//         } 
-//     })
-
-// })
-
-
-
 router.post('/vali',function(req,res){
     var choice=req.body.vote;
 
@@ -88,10 +56,7 @@ connection.query('UPDATE vote set voteCount=voteCount+1 where userId=?',[choice]
 })
 
 })
-
-
-
- router.get('/adminlogin',function(req,res){
+router.get('/adminlogin',function(req,res){
     console.log("WELCOME!!");
     res.render("alogin");
 })
@@ -103,7 +68,6 @@ router.post('/advali',function(req,res){
       }, 2000);
     });
   }
-  
   async function asyncCall() {
     const result = await resolveAfter2Seconds();
     console.log(result);
@@ -134,10 +98,4 @@ router.get('/index',function(req,res){
     console.log("WELCOME!!");
     res.render("index");
 })
-
-
-
-
-
-
 module.exports=router;
